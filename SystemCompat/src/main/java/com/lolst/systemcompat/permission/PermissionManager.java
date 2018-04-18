ea372bbbf90e2c71d987e2eece02238721e36ff7
lailele <452612367@qq.com>
2018-04-18 17:51:45 +0800

    修改函数访问权限

--- SystemCompat/src/main/java/com/lolst/systemcompat/permission/PermissionManager.java
+++ SystemCompat/src/main/java/com/lolst/systemcompat/permission/PermissionManager.java
@@ -151,7 +151,7 @@ public class PermissionManager {
      * 跳转到本应用的授权列表
      * @param activity
      */
-    private void startInstallPermissionSettingActivity(Activity activity) {
+    public static void startInstallPermissionSettingActivity(Activity activity) {
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
             //注意这个是8.0新API
             // 注意：当通过Intent 跳转到未知应用授权列表的时候，一定要加上包名，这样就能直接跳转到你的app下，不然只能跳转到列表。
